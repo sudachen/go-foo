00b2fa2b1618db821f37bb81cd152a776611059f
Alexey Sudachen <alexey.sudachen@vacasa.com>
2020-02-07 17:59:30 -0300

    updated

--- lazy/collect.go
+++ lazy/collect.go
@@ -3,11 +3,11 @@ package lazy
 import "reflect"
 
 func (z *Stream) Collect() interface{} {
-	r := reflect.MakeSlice(reflect.SliceOf(z.Tp),0,0)
+	r := reflect.MakeSlice(reflect.SliceOf(z.Tp), 0, 0)
 	return r.Interface()
 }
 
 func (z *Stream) ConqCollect(concurrency int) interface{} {
-	r := reflect.MakeSlice(reflect.SliceOf(z.Tp),0,0)
+	r := reflect.MakeSlice(reflect.SliceOf(z.Tp), 0, 0)
 	return r.Interface()
 }
