71fc0ba497ccdfd2cda90ab3ec626aba9358ed7c
Alexey Sudachen <alexey.sudachen@vacasa.com>
2020-02-16 19:56:54 -0300

    updated

---
+++ lazy/option.go
@@ -0,0 +1,32 @@
+package lazy
+
+import (
+	"reflect"
+)
+
+func Option(t interface{}, o []interface{}) reflect.Value {
+	tv := reflect.ValueOf(t)
+	for _, x := range o {
+		v := reflect.ValueOf(x)
+		if v.Type() == tv.Type() {
+			return v
+		}
+	}
+	return tv
+}
+
+func StrOption(t interface{}, o []interface{}) string {
+	return Option(t, o).String()
+}
+
+func IntOption(t interface{}, o []interface{}) int {
+	return int(Option(t, o).Int())
+}
+
+func FloatOption(t interface{}, o []interface{}) float64 {
+	return Option(t, o).Float()
+}
+
+func BoolOption(t interface{}, o []interface{}) bool {
+	return Option(t, o).Bool()
+}
