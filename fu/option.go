7c5fbafbab76f8368ce3115ca2934a8bc64ea30f
Alexey Sudachen <alexey@sudachen.name>
2020-02-21 02:02:11 -0300

    new option functions

--- fu/option.go
+++ fu/option.go
@@ -15,6 +15,10 @@ func Option(t interface{}, o []interface{}) reflect.Value {
 	return tv
 }
 
+func IfsOption(t interface{}, o []interface{}) interface{} {
+	return Option(t,o).Interface()
+}
+
 func StrOption(t interface{}, o []interface{}) string {
 	return Option(t, o).String()
 }
@@ -29,4 +33,34 @@ func FloatOption(t interface{}, o []interface{}) float64 {
 
 func BoolOption(t interface{}, o []interface{}) bool {
 	return Option(t, o).Bool()
+}
+
+func MultiOption(o []interface{}, t ...interface{}) (reflect.Value, int) {
+	for _, x := range o {
+		for i, tv := range t {
+			v := reflect.ValueOf(x)
+			if v.Type() == reflect.TypeOf(tv) {
+				return v, i
+			}
+		}
+	}
+	return reflect.ValueOf(t[0]),0
+}
+
+func StrMultiOption(o []interface{}, t ...interface{}) (string, int) {
+	v, i := MultiOption(o, t...)
+	return v.String(), i
+}
+
+func AllStrOptions(o []interface{}, t ...interface{}) []string {
+	r := []string{}
+	for _, x := range o {
+		for _, tv := range t {
+			v := reflect.ValueOf(x)
+			if v.Type() == reflect.TypeOf(tv) {
+				r = append(r,v.String())
+			}
+		}
+	}
+	return r
 }
